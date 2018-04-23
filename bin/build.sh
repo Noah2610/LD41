f85311b72b3dba0eb6993efc1347d26c513c7aca
Noah <rosenzweig.noah@gmail.com>
2018-04-23 02:24:08 +0200

    Update bin/build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -6,6 +6,15 @@ gemfile='./Gemfile'
 output_file='./LD41.exe'
 IFS=$'\n'
 other_files=('./settings.yml')
-other_files_find=($( find ./src -type f ))
+other_files_find=($(    \
+	find ./src -type f -o \
+		-iname '*.rb'    -o \
+		-iname '*.png'   -o \
+		-iname '*.jpg'   -o \
+		-iname '*.jpeg'  -o \
+		-iname '*.wav'   -o \
+		-iname '*.ttf'   -o \
+		-iname '*.yml'      \
+))
 
 ocra $entry_file ${other_files[@]} ${other_files_find[@]} --windows --gemfile $gemfile --output $output_file
